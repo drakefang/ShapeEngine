37e6913984f5e554d8093fa0d913cd629405b1ce
yuyz.ccnu@live.com <yuyz.ccnu@live.com>
2025-02-23 14:51:43 +0800

    add raylib

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
+
+#include "raylib.h"
+
+
 int main(int argc, char** argv)
-{
+{    
+    SetConfigFlags(FLAG_WINDOW_RESIZABLE | FLAG_VSYNC_HINT | FLAG_WINDOW_HIGHDPI);
+    InitWindow(1024, 768, "ShapeGame");
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(SKYBLUE);
+        EndDrawing();
+    }
+    CloseWindow();
     return 0;
 }
