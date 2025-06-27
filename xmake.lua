4b5be9d57f0562b6e8c974ed76cfcaa00886715c
yuyz.ccnu@live.com <yuyz.ccnu@live.com>
2025-06-27 22:22:56 +0800

    update

--- xmake.lua
+++ xmake.lua
@@ -13,20 +13,6 @@ local requires = {
 }
 add_requires(requires)
 
-target(project_name)
-    set_kind("binary")
-    add_packages("raylib")
-    add_packages("entt")
-    add_packages("glm")
-    add_packages("raygui")
-
-    add_includedirs("src")
-    add_includedirs("src/Core")
-
-    add_headerfiles("src/**/*.h")
-    add_files("src/main.cpp")
-    add_files("src/**/*.cpp")
-
 local test_proj_name = "test_raylib"
 target(test_proj_name)
     set_kind("binary")
