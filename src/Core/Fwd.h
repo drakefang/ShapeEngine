3181ee3685604a3362492e5dc2d45b64fe8dd6fe
yuyz <yuyz.ccnu@live.com>
2025-06-27 21:40:11 +0800

    add new object

--- src/Core/Fwd.h
+++
@@ -1,9 +0,0 @@
-
-#pragma once
-
-#include "entt/core/fwd.hpp"
-
-namespace ShapeGame
-{
-    using id_type = entt::id_type;
-}
---
+++ src/Core/Object.h
@@ -0,0 +1,11 @@
+
+#pragma once
+
+
+namespace ShapeGame
+{
+    class Object
+    {
+        
+    };
+}
