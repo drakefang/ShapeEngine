7db772aeefd3345aa14fec2fa7636c41ba2e6b0a
yuyz.ccnu@live.com <yuyz.ccnu@live.com>
2025-07-23 23:05:17 +0800

    update

--- src/ShapeEngine/App/IPrimaryGameModule.h
+++ src/ShapeEngine/App/IPrimaryGameModule.h
@@ -9,6 +9,22 @@ namespace ShapeEngine
 {
     class IPrimaryGameModule : public IModuleInterface
     {
+    public:
+        void Startup() override
+        {
+        }
 
+        void Shutdown() override
+        {
+        }
+
+        virtual void Tick(float DeltaTimes) = 0;
+
+        [[nodiscard]] virtual std::string GetPrimaryGameModuleName() const = 0;
+
+        [[nodiscard]] virtual std::vector<std::string> GetPrimaryGameModuleDependencies() const
+        {
+            return {};
+        }
     };
 }
